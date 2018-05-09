4bda7862968dd6f18a9f4d1313fcce02dd76af3e
Ayush Goyal <ayush987goyal@gmail.com>
2018-05-09 23:10:35 +0530

    add verify chain

--- blockchain.py
+++ blockchain.py
@@ -36,12 +36,31 @@ def print_blockchain_elements():
     for block in blockchain:
         print('Outputting block')
         print(block)
+    else:
+        print('_' * 20)
 
 
-while True:
+def verify_chain():
+    is_valid = True
+    for block_index in range(len(blockchain)):
+        if block_index == 0:
+            continue
+        elif blockchain[block_index][0] == blockchain[block_index - 1]:
+            is_valid = True
+        else:
+            is_valid = False
+            break
+
+    return is_valid
+
+
+waiting_for_input = True
+
+while waiting_for_input:
     print('Please choose')
     print('1: Add a new transaction value')
     print('2: Output the blockchain blocks')
+    print('h: Manipulate the chain')
     print('q: Quit')
     user_choice = get_user_choice()
 
@@ -50,9 +69,18 @@ while True:
         add_transaction(tx_amt, get_last_blockchain_value())
     elif user_choice == '2':
         print_blockchain_elements()
+    elif user_choice == 'h':
+        if len(blockchain) > 0:
+            blockchain[0] = [2]
     elif user_choice == 'q':
-        break
+        waiting_for_input = False
     else:
         print('Input was invalid, please pick a value from the list!')
+
+    if not verify_chain():
+        print('Invalid blockchain!')
+        break
+else:
+    print('User left!')
 
 print('Done!')
