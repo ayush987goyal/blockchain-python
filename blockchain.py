0cb93a3c0e901d88a0743ca2ab1fd6a0377c0ed3
Ayush Goyal <ayush987goyal@gmail.com>
2018-05-09 22:46:44 +0530

    adding conditionals

--- blockchain.py
+++ blockchain.py
@@ -5,33 +5,54 @@ blockchain = []
 def get_last_blockchain_value():
     ''' Returns the last value of the current blockchain.
     '''
+    if len(blockchain) < 1:
+        return None
+
     return blockchain[-1]
 
 
-def add_value(transaction_amount, last_transaction=[1]):
+def add_transaction(transaction_amount, last_transaction=[1]):
     ''' Appends a new value as well as last value to the current blockchain
 
     Arguments:
         :transaction_amount: The amount that should be added.
         :last_transaction: The last blockchain transaction (default [1]).
     '''
+    if last_transaction == None:
+        last_transaction = [1]
     blockchain.append([last_transaction, transaction_amount])
 
 
-def get_user_input():
+def get_transaction_value():
     return float(input('Your transaction amout please: '))
 
 
-tx_amt = get_user_input()
-add_value(tx_amt)
+def get_user_choice():
+    return input('Your choice: ')
 
-while True:
-    tx_amt = get_user_input()
-    add_value(tx_amt, get_last_blockchain_value())
 
+def print_blockchain_elements():
     # Output the blockchain list to the console
     for block in blockchain:
         print('Outputting block')
         print(block)
+
+
+while True:
+    print('Please choose')
+    print('1: Add a new transaction value')
+    print('2: Output the blockchain blocks')
+    print('q: Quit')
+    user_choice = get_user_choice()
+
+    if user_choice == '1':
+        tx_amt = get_transaction_value()
+        add_transaction(tx_amt, get_last_blockchain_value())
+    elif user_choice == '2':
+        print_blockchain_elements()
+    elif user_choice == 'q':
+        break
+    else:
+        print('Input was invalid, please pick a value from the list!')
 
 print('Done!')
