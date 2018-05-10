69b383f4d5c1f83ce3d0edbeb31659e45f002900
Ayush Goyal <ayush987goyal@gmail.com>
2018-05-10 22:40:37 +0530

    add mine block logic

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,10 @@
 # Initializing our blockchain list
-blockchain = []
+genesis_block = {
+    'previous_hash': '',
+    'index': 0,
+    'transactions': []
+}
+blockchain = [genesis_block]
 open_transactions = []
 owner = 'Ayush'
 
@@ -30,7 +35,16 @@ def add_transaction(recipient, sender=owner, amount=1.0):
 
 
 def mine_block():
-    pass
+    last_block = blockchain[-1]
+    hashed_block = '-'.join([str(last_block[key]) for key in last_block])
+
+    print(hashed_block)
+    block = {
+        'previous_hash': hashed_block,
+        'index': len(blockchain),
+        'transactions': open_transactions
+    }
+    blockchain.append(block)
 
 
 def get_transaction_value():
@@ -71,7 +85,8 @@ waiting_for_input = True
 while waiting_for_input:
     print('Please choose')
     print('1: Add a new transaction value')
-    print('2: Output the blockchain blocks')
+    print('2: Mine a new block')
+    print('3: Output the blockchain blocks')
     print('h: Manipulate the chain')
     print('q: Quit')
     user_choice = get_user_choice()
@@ -82,6 +97,8 @@ while waiting_for_input:
         add_transaction(recipient, amount=amount)
         print(open_transactions)
     elif user_choice == '2':
+        mine_block()
+    elif user_choice == '3':
         print_blockchain_elements()
     elif user_choice == 'h':
         if len(blockchain) > 0:
@@ -91,9 +108,9 @@ while waiting_for_input:
     else:
         print('Input was invalid, please pick a value from the list!')
 
-    if not verify_chain():
-        print('Invalid blockchain!')
-        break
+    # if not verify_chain():
+    #     print('Invalid blockchain!')
+    #     break
 else:
     print('User left!')
 
