21e4e18a26e70f7304f65ef4ab1a7df0655c0cfb
Ayush Goyal <ayush987goyal@gmail.com>
2018-05-11 22:38:20 +0530

    add new hash method

--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,6 @@
-import functools
+from functools import reduce
+import hashlib
+import json
 # The reward we give to miners
 MINING_REWARD = 10
 
@@ -24,7 +26,7 @@ def hash_block(block):
     Arguments:
         :block: The block that should be hashed.
     """
-    return '-'.join([str(block[key]) for key in block])
+    return hashlib.sha256(json.dumps(block).encode()).hexdigest()
 
 
 def get_balance(participant):
@@ -38,12 +40,12 @@ def get_balance(participant):
     open_tx_sender = [tx['amount']
                       for tx in open_transactions if tx['sender'] == participant]
     tx_sender.append(open_tx_sender)
-    amount_sent = functools.reduce(
+    amount_sent = reduce(
         lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_sender, 0)
 
     tx_recipient = [[tx['amount'] for tx in block['transactions']
                      if tx['recipient'] == participant] for block in blockchain]
-    amount_recieved = functools.reduce(
+    amount_recieved = reduce(
         lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_recipient, 0)
 
     return amount_recieved - amount_sent
