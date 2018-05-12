91180c630b99c6e535f159f336356eac6f37ff89
Ayush Goyal <ayush987goyal@gmail.com>
2018-05-12 22:16:07 +0530

    add routes for chain adn mine

--- blockchain.py
+++ blockchain.py
@@ -147,7 +147,7 @@ class Blockchain:
     def mine_block(self):
         """Create a new block and add open transactions to it."""
         if self.hosting_node == None:
-            return False
+            return None
 
         last_block = self.__chain[-1]
         hashed_block = hash_block(last_block)
@@ -164,7 +164,7 @@ class Blockchain:
 
         for tx in copied_transactions:
             if not Wallet.verify_transaction(tx):
-                return False
+                return None
         copied_transactions.append(reward_transaction)
         block = Block(len(self.__chain), hashed_block,
                       copied_transactions, proof)
@@ -172,4 +172,4 @@ class Blockchain:
         self.__chain.append(block)
         self.__open_transactions = []
         self.save_data()
-        return True
+        return block
